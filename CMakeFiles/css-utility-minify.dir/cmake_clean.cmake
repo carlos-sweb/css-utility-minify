ca1770af3856f30b595bd0c83436fe6562b8969e
carlos-sweb <c4rl0sill3sc4@gmail.com>
2021-12-28 00:01:57 -0300

    update

---
+++ CMakeFiles/css-utility-minify.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/css-utility-minify.dir/css-utility-minify.c.o"
+  "css-utility-minify"
+  "css-utility-minify.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/css-utility-minify.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
